b3a0c739245fa19a8bcba1a243db96befd2c1064
creatking <522736228@qq.com>
2017-04-15 20:37:29 +0800

    添加新内容

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxActivity.java
@@ -2,12 +2,14 @@ package com.bignerdranch.android.beatbox;
 
 import android.app.Activity;
 import android.os.Bundle;
+//添加新内容
 
 public class BeatBoxActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.fragment_beat_box);
     }
 }
